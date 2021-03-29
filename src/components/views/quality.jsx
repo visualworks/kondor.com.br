a2e9f98f4befedb7d05ea115930816fc03d8c9bc
Henrique Mattos <henrique.mattos@sap.com>
2021-03-29 18:30:22 +0100

    Update Quality texts based on client request
    E-mail thread "ENC: CORREÇÃO/ATUALIZAÇÃO DO SITE KONDOR" from Mar 25, 2021, 5:29 PM between Marcello Barbosa and Henrique Mattos.

--- src/components/views/quality.jsx
+++ src/components/views/quality.jsx
@@ -36,18 +36,17 @@ export default class ViewProducts extends App {
                 </div>
             </div>
             <h2 className="title">Certificações</h2>
-            <p>Estamos orgulhosos com os resultados! A KONDOR tem difundido a consciência ambiental e noções de que legislações e normas devem ser rigorosamente cumpridas para minimizar e evitar impactos e reações de degradação ao meio ambiente, que poderiam ser provocadas por atividades industriais. Continuaremos sempre nessa busca pois acreditamos num futuro melhor para as futuras gerações. Busca pela qualidade, atender necessidades e satisfazer o cliente significa fabricar produtos que respondam e superem quaisquer expectativas. Para a KONDOR é indispensável dispor de processos confiáveis, previsíveis e guiados por boas práticas gerenciais, garantindo assim um ótimo nível de excelência. Nesse sentido a KONDOR está sempre em busca de atualização por processos de melhoria e qualidade.</p>
-            <p>A Kondor está em processo de certificação da nova versão da ISO 14001:2015 com o escopo “Prestação de Serviços de Usinagem e Fabricação de Peças para a Indústria Automotiva e Mecânica em Geral”, com previsão para Março/2017.</p>
+            <p>Estamos orgulhosos que a Kondor se certificou nas principais normas da qualidade, isto evidencia que não zela apenas pela qualidade de seus produtos, mas zela também pelo cumprimento de regulamentos, legislações e os requisitos do cliente. E, demonstra que a gestão da qualidade não é apenas a aplicação de regras novas em sua cadeia produtiva, mas o empenho de construir uma nova cultura organizacional e melhorá-la continuamente, envolvendo a alta administração, colaboradores, fornecedores e cliente. Toda essa nova estrutura obtida por meio das certificações tem como principal objetivo a satisfação do cliente.</p>
+            <p>A certificação na norma ISO 14001 é outro ponto de orgulho, pois demonstra aprovação às diversas ações realizadas no dia a dia com o intuito de proteger o meio ambiente, ou seja, impossibilitar que os aspectos ambientais tornem-se impactos ambientais.</p>
             <hr />
             <h2 className="title">Política Integrada - Qualidade e Meio Ambiente</h2>
-            <p>A INDÚSTRIA MECÂNICA KONDOR LTDA. atua no fornecimento de peças acabadas e prestação de serviços de usinagem de alta precisão destinadas às montadoras de veículos pesados, ao setor automotivo e outros.</p>
-            <p>Declara seu comprometimento com o Meio Ambiente, atendimento aos requisitos e satisfação dos seus clientes, por meio de:</p>
+            <p>A <strong>Indústria Mecânica Kondor Ltda.</strong> atua no fornecimento de peças acabadas e prestação de serviços de usinagem de alta precisão destinadas às montadoras de veículos pesados, ao setor automotivo e outros.</p>
+            <p>Declara seu comprometimento com o meio ambiente e satisfação dos seus clientes por meio de:</p>
             <ul>
+                <li>Proteção ao meio ambiente: prevenindo a poluição, atendendo à legislação e às outras partes interessadas, e reduzindo o consumo dos recursos naturais;</li>
                 <li>Atendimento aos requisitos da qualidade, serviços fornecidos e prazos de entrega dos produtos;</li>
-                <li>Treinamento de seus colaboradores em todos os níveis hierárquicos;</li>
-                <li>Melhoria contínua dos processos do sistema de gestão;</li>
-                <li>Redução da utilização dos recursos naturais;</li>
-                <li>Preservação do meio ambiente – respeitando a legislação, as outras partes interessadas e evitando a poluição.</li>
+                <li>Melhoria contínua do sistema de gestão integrado;</li>
+                <li>Treinamento de seus funcionários em todos os níveis hierárquicos.</li>
             </ul>
             <p>Kazunari Okimasu<br />Presidente</p>
         </div>;
